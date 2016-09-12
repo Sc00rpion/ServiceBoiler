95a6aa0d0b7676a0a716e38c2842bd2a8636e3ca
Maciej <Maciej@DESKTOP-RHCNP97>
2016-09-12 18:58:32 +0200

    Adding a method to the search the boiler after the date of service and test

--- ServiceBoilers/src/main/java/database/BoilerRepository.java
+++ ServiceBoilers/src/main/java/database/BoilerRepository.java
@@ -1,9 +1,19 @@
 package database;
 
-import org.springframework.data.repository.CrudRepository;
+import java.sql.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import entity.Boiler;
 
-public interface BoilerRepository extends CrudRepository<Boiler, Long> {
+public interface BoilerRepository extends JpaRepository<Boiler, Long> {
+	
+	List<Boiler> findByAddressCity(String city);
+	
+	@Query("select b from Boiler b where (select max(s.dateOfService) from Boiler c join c.service s where c.id = b.id) between :start and :end")
+	List<Boiler> findByService(@Param("start") Date start, @Param("end") Date end);
 
 }
--- ServiceBoilers/src/main/java/database/TestJpa.java
+++ ServiceBoilers/src/main/java/database/TestJpa.java
@@ -1,20 +1,36 @@
 package database;
 
 import java.sql.Date;
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import entity.Address;
 import entity.Boiler;
+import entity.Service;
 
 public class TestJpa {
 	
 	   public static void main(String[] args) {
 		   ApplicationContext context = new AnnotationConfigApplicationContext(DataSourceConfiguration.class);
 		   BoilerRepository rep = context.getBean(BoilerRepository.class);
-		   rep.save(new Boiler("kocio³", new Date(19999999999l)));
-		   Iterable<Boiler> boilers = rep.findAll();
+		   
+		   Address a1 = new Address();
+		   Boiler b1 = new Boiler("kocio³", new Date(19999999999l),a1);
+		   Set<Service> s1 = b1.getService();
+		   s1.add(new Service(new Date(3000000000l)));
+		   s1.add(new Service(new Date(1000000000l)));
+		   s1.add(new Service(new Date(2000000000l)));
+		   rep.save(b1);
+		   Address a2 = new Address();
+		   a2.setCity("warszawa");
+		   Boiler b2 = new Boiler("kocio³", new Date(19999999999l),a2);
+		   Set<Service> s2 = b2.getService();
+		   s2.add(new Service(new Date(3000000000l)));
+		   s2.add(new Service(new Date(4000000000l)));
+		   rep.save(b2);		 
+		   Iterable<Boiler> boilers = rep.findByService(new Date(3500000000l),new Date(5000000000l));
 		   for (Boiler boiler : boilers) {
 			System.out.println(boiler);
 		}
